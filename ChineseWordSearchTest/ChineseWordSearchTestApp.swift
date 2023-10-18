4e7f97f98d35be82a1b06f21577b15a9dad991a9
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2023-10-18 20:26:47 +0800

    Initial Commit

---
+++ ChineseWordSearchTest/ChineseWordSearchTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChineseWordSearchTestApp.swift
+//  ChineseWordSearchTest
+//
+//  Created by Kai Quan Tay on 18/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ChineseWordSearchTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChineseWordSearchTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChineseWordSearchTest
+//
+//  Created by Kai Quan Tay on 18/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
